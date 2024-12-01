cff75ff6e3b3758d04b3d99de092c83210f7b484
TadeuN1 <tadeunrs@gmail.com>
2024-12-01 19:22:46 -0300

    return to study

--- section08/carrinho/src/pages/cart/index.tsx
+++ section08/carrinho/src/pages/cart/index.tsx
@@ -1,33 +1,54 @@
+import { useContext } from "react"
+import { CartContext } from "../../contexts/CartContext"
+import { Link } from "react-router-dom"
 
 export function Cart(){
+
+    const {cart, total, addItemCart, removeItemCart} = useContext(CartContext)
     return(
         <div className="w-full max-w-7xl mx-auto">
             <h1 className="font-medium text-2xl text-center my-5">Meu carrinho</h1>
 
-
-            <section className="flex items-center justify-between border-b-2 border-gray-300">
-                <img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/airpods-4-anc-select-202409_FV1_FMT_WHH?wid=752&hei=636&fmt=jpeg&qlt=90&.v=1725502553451"
-                 alt="Logo do produto"
+            {cart.length === 0 && (
+                <div className="flex flex-col items-center justify-center">
+                    <p className="font-medium"> Seu carrinho está vazio... </p>
+                    <Link to="/"
+                    className="bg-slate-600 my-3 p-1 px-3 text-white font-medium rounded"
+                    >
+                        Acessar produtos
+                    </Link>
+                </div>
+            )}
+            {cart.map( (item) => (
+                <section key={item.id} className="flex items-center justify-between border-b-2 border-gray-300">
+                <img src={item.cover}
+                 alt={item.title}
                 className="w-28"
                 />
-                <strong> Preço: R$1000</strong>
+                <strong> Preço: {item.price}</strong>
                 <div className="flex items-center justify-center gap-3">
                     <button 
+                    onClick={() => removeItemCart(item)}
                     className="bg-slate-600 px-2 rounded text-white flex items-center justify-center"> 
                     - </button>
-                    1
-                    <button 
+                    {item.amount}
+                    <button
+                    onClick={ () => addItemCart(item)} 
                     className="bg-slate-600 px-2 rounded text-white flex items-center justify-center"> 
                     + </button>
                 
                 </div>
 
                 <strong className="float-right">
-                    SubTotal: R$1000
+                    SubTotal: {item.total.toLocaleString("pt-BR", {
+                        style: "currency",
+                        currency: "BRL"
+                    })}
                 </strong>
             </section>
+            ))}
 
-            <p className="font-bold mt-4">Total: R$1000</p>
+           {cart.length !== 0 &&  <p className="font-bold mt-4">Total: {total}</p>}
         </div>
     )
 }
--- section08/carrinho/src/pages/home/index.tsx
+++ section08/carrinho/src/pages/home/index.tsx
@@ -1,8 +1,9 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useContext } from "react"
 import { BsCartPlus } from "react-icons/bs"
 import { api } from "../../services/api"
+import { CartContext } from "../../contexts/CartContext";
 
-interface ProductProps{
+ export interface ProductProps{
     id: number;
     title: string;
     description: string;
@@ -11,7 +12,7 @@ interface ProductProps{
 }
 
 export function Home(){
-
+    const {addItemCart} = useContext(CartContext)
     const [products, setProducts] = useState<ProductProps[]>([])
 
     useEffect(() => {
@@ -22,6 +23,10 @@ export function Home(){
 
         getProducts();
     }, [])
+
+    function handleAddCartItem(product: ProductProps){
+        addItemCart(product)
+    }
 
     return(
         <main className="w-full max-w-7xl px-4 mx-auto">
@@ -41,7 +46,8 @@ export function Home(){
                             style: "currency",
                             currency: "BRL"
                         })} </strong>
-                        <button className="bg-zinc-900 p-1 rounded">
+                        <button className="bg-zinc-900 p-1 rounded"
+                        onClick={ () =>  handleAddCartItem(product)}>
                             <BsCartPlus size={20} color="#FFF"/>
                         </button>
                      </div>
